f60958af36880db6f6fdbbf53918c4d85c1fb9d9
Daniel Levitt <daniel.levitt@guardian.co.uk>
2014-05-06 17:35:38 +0100

    Added check for blank new lines in tourDates.txt

--- js/main.js
+++ js/main.js
@@ -1,45 +1,47 @@
-$(function () {
+$(function() {
 
-    var tourTable = $('table#tourTable tbody');
+  var tourTable = $('table#tourTable tbody');
 
-    $.ajax({
-        url: 'tourDates.txt',
-    }).done(function (data) {
+  $.ajax({
+    url: 'tourDates.txt',
+  }).done(function(data) {
 
+    var tourTableHtml = [],
+      dates = data.split('\n');
 
-        var tourTableHtml = [],
-            dates = data.split('\n');
-        $.each(dates, function (i, o) {
-            var tourDate = o.split('//'),
-                tdate = tourDate[0].trim(),
-                tplace = tourDate[1].trim(),
-                country = tourDate[2].trim(),
-                tourTableRow = [];
+    $.each(dates, function(i, o) {
+      if (o.length) {
+        var tourDate = o.split('//'),
+          tdate = tourDate[0].trim(),
+          tplace = tourDate[1].trim(),
+          country = tourDate[2].trim(),
+          tourTableRow = [];
 
-            tourTableRow.push(tdate);
-            tourTableRow.push(tplace + ', <span class="country">' + country + '</span>');
+        tourTableRow.push(tdate);
+        tourTableRow.push(tplace + ', <span class="country">' + country + '</span>');
 
-            tourTableHtml.push( '<td>' + tourTableRow.join('</td><td>') + '</td>' );
-        });
-
-        tourTable.append( '<tr>' + tourTableHtml.join('</tr><tr>') + '</tr>' );
+        tourTableHtml.push('<td>' + tourTableRow.join('</td><td>') + '</td>');
+      }
     });
+
+    tourTable.append('<tr>' + tourTableHtml.join('</tr><tr>') + '</tr>');
+  });
 });
 
 // https://dev.twitter.com/docs/intents#follow-intent
 (function() {
   if (window.__twitterIntentHandler) return;
   var intentRegex = /twitter\.com(\:\d{2,4})?\/intent\/(\w+)/,
-      windowOptions = 'scrollbars=yes,resizable=yes,toolbar=no,location=yes',
-      width = 550,
-      height = 420,
-      winHeight = screen.height,
-      winWidth = screen.width;
+    windowOptions = 'scrollbars=yes,resizable=yes,toolbar=no,location=yes',
+    width = 550,
+    height = 420,
+    winHeight = screen.height,
+    winWidth = screen.width;
 
   function handleIntent(e) {
     e = e || window.event;
     var target = e.target || e.srcElement,
-        m, left, top;
+      m, left, top;
 
     while (target && target.nodeName.toLowerCase() !== 'a') {
       target = target.parentNode;
@@ -56,7 +58,7 @@ $(function () {
         }
 
         window.open(target.href, 'intent', windowOptions + ',width=' + width +
-                                           ',height=' + height + ',left=' + left + ',top=' + top);
+          ',height=' + height + ',left=' + left + ',top=' + top);
         e.returnValue = false;
         e.preventDefault && e.preventDefault();
       }
